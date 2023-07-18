2c1aeeb1b83ca3342b4212e40fe208d9e7a63244
Claudio Santibanez <yeyecasb@hotmail.com>
2023-07-18 14:30:45 -0400

    commit

--- carrullanca/settings.py
+++ carrullanca/settings.py
@@ -84,12 +84,8 @@ WSGI_APPLICATION = 'carrullanca.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':'django.db.backends.postgresql',
-        'NAME':'CARRULLANCA',
-        'USER':'postgres',
-        'PASSWORD':'admin',
-        'HOST':'localhost',
-        'PORT':'5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
